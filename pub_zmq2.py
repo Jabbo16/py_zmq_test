d752e755608ea32c912adcd799ec9f87d921f578
Javier Sacido <jabbo16@gmail.com>
2021-03-25 09:18:32 +0100

    test connection

--- pub_zmq2.py
+++ pub_zmq2.py
@@ -8,6 +8,8 @@ ip = sys.argv[0]
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
+socket.bind("tcp://172.21.0.1:8000")
+socket.connect("tcp://172.21.0.1:8000")
 socket.connect("tcp://%s:%s" % (ip, port))
 
 topic = 0
