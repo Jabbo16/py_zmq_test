90084fc8fb107932589537290e9abc879d20cfac
Javier Sacido <jabbo16@gmail.com>
2021-03-25 10:04:55 +0100

    working config with nodePort, updated pub.py to python3

--- pub_zmq2.py
+++ pub_zmq2.py
@@ -3,20 +3,21 @@ import random
 import time
 import sys
 
-port = sys.argv[1]
-ip = sys.argv[0]
+port = sys.argv[2]
+ip = sys.argv[1]
+
+address = "tcp://{}:{}".format(ip, port)
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://172.21.0.1:8000")
-socket.connect("tcp://172.21.0.1:8000")
-socket.connect("tcp://%s:%s" % (ip, port))
+print(address)
+socket.connect(address)
 
 topic = 0
 
 while True:
     topic = topic + 1
     messagedata = topic
-    print "Message sent: %d %d" % (topic, messagedata)
-    socket.send("%d %d" % (topic, messagedata))
+    print("Message sent: %d %d" % (topic, messagedata))
+    socket.send_string("%d %d" % (topic, messagedata))
     time.sleep(1)
